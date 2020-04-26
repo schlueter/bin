8207f0d45935f5c6483f4ad3e3cfedab658346c4
Schlueter <b@schlueter.blue>
2020-04-26 09:47:14 -0700

    Add launch-polybar for xmonad et al

---
+++ launch-polybar.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+exec > >(tee -a "$XDG_CONFIG_HOME/polybar.log") 2>&1
+
+echo "Terminating any current polybar instances..." >&2
+for fifo in .xmonad-workspace-log .xmonad-title-log
+do
+    if ! [ -p "/tmp/$fifo" ]
+    then
+        mkfifo "/tmp/$fifo"
+    fi
+done
+# Terminate already running bar instances
+if ! killall -q -s SIGUSR1 polybar
+then
+    polybar main >>"$XDG_CONFIG_HOME/polybar.log"
+    echo "Polybar successfully started." >&2
+else
+    echo "Polybar reloaded."
+fi
