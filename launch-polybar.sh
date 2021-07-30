720ec9993085482aec145b791cf6bcd4b25ccb23
Schlueter <b@schlueter.blue>
2021-07-30 10:52:27 -0400

    Update launch-polybar.sh

--- launch-polybar.sh
+++ launch-polybar.sh
@@ -6,6 +6,10 @@ exec > >(tee -a "$XDG_CONFIG_HOME/polybar.log") 2>&1
 echo "Terminating any current polybar instances..." >&2
 for fifo in .xmonad-workspace-log .xmonad-title-log
 do
+    if [ -f "/tmp/$fifo" ]
+    then
+        rm "/tmp/$fifo"
+    fi
     if ! [ -p "/tmp/$fifo" ]
     then
         mkfifo "/tmp/$fifo"
